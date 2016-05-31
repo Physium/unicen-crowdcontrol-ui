e35d7d93f3c0de9569e3acf37a6397c628706811
physium <wjloh91@gmail.com>
2016-05-31 10:23:41 +0800

    fix minor chart issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ console.log('\'Allo \'Allo!');
 google.charts.load('current', { 'packages': ['line', 'corechart'] });
 var urlStatus = 'http://unicern-rpc-03:8080/SimulatorControl/?command=status';
 var urlStart = 'http://unicern-rpc-03:8080/SimulatorControl/?command=start';
-var urlStop = 'http://unicern-rpc-03:8080/SimulatorControl/?command=stop';
+var urlStop = 'http://unicern-rpc-03:8080/SimulatorControl/?command=urlStop';
 var urlData = 'http://unicern-rpc-03:8080/SimulatorControl/?command=getPercentileData';
 //10.1.12.68
 
@@ -46,7 +46,7 @@ $(document).ready(function() {
     });
 
     //onclick test
-    $("#test").click(function() {
+    $('#test').click(function() {
         $.get(urlStatus, function(data) {
             console.log(data);
             $('.jumbotron').show();
@@ -58,7 +58,7 @@ $(document).ready(function() {
     });
 
     //onclick start
-    $("#start").click(function() {
+    $('#start').click(function() {
         $.get(urlStart, function(data) {
             console.log(data);
             $('.jumbotron').show();
@@ -70,7 +70,7 @@ $(document).ready(function() {
     });
 
     //onclick stop
-    $("#stop").click(function() {
+    $('#stop').click(function() {
         $.get(urlStop, function(data) {
             console.log(data);
             $('.jumbotron').show();
@@ -83,93 +83,93 @@ $(document).ready(function() {
 
     //END APIs Calls
     //select2 elements
-    $(".js-example-basic-single").select2();
+    $('.js-example-basic-single').select2();
 
     //datetimepicker
-    $("#datetimepicker3").datetimepicker({
+    $('#datetimepicker3').datetimepicker({
         format: 'HH:mm'
     });
 
     //nav-tab click events
-    $("#simulationChartTab").click(function() {
-        $('#simulation').load("simulationLineChart.html");
+    $('#simulationChartTab').click(function() {
+        $('#simulation').load('simulationLineChart.html');
     });
-    $("#simulationTableTab").click(function() {
-        $('#simulation').load("simulationTable.html");
+    $('#simulationTableTab').click(function() {
+        $('#simulation').load('simulationTable.html');
     });
-    $("#simulationEvacuationChartTab").click(function() {
-            $('#simulation').load("simulationEvacuationChart.html")
+    $('#simulationEvacuationChartTab').click(function() {
+            $('#simulation').load('simulationEvacuationChart.html')
         })
         //initial load
-    $("#simulation").load("simulationLineChart.html");
+    $('#simulation').load('simulationLineChart.html');
 
     //form values
-    console.log($("#building select").val());
-    console.log($("#location").val());
-    console.log($("#time input").val());
-    console.log($("#crowd select").val());
-    console.log($("#floor select").val());
-    console.log($("#floor input:checked").val());
-    console.log($("#escalator select").val());
-    console.log($("#escalator input:checked").val());
-    console.log($("#access select").val());
-    console.log($("#access input:checked").val());
+    console.log($('#building select').val());
+    console.log($('#location').val());
+    console.log($('#time input').val());
+    console.log($('#crowd select').val());
+    console.log($('#floor select').val());
+    console.log($('#floor input:checked').val());
+    console.log($('#escalator select').val());
+    console.log($('#escalator input:checked').val());
+    console.log($('#access select').val());
+    console.log($('#access input:checked').val());
 
     //submit
-    $("#run").click(function() {
-        var building = $("#building select").val();
-        var location = $("#location select").val();
-        var time = $("#time input").val();
-        var crowd = $("#crowd select").val();
-        var lift = $("#lift select").val();
-        var escalator = $("#escalator select").val();
-        var access = $("#access select").val();
-        var information = $("#information select").val();
-        var path = $("#path select").val();
+    $('#run').click(function() {
+        var building = $('#building select').val();
+        var location = $('#location select').val();
+        var time = $('#time input').val();
+        var crowd = $('#crowd select').val();
+        var lift = $('#lift select').val();
+        var escalator = $('#escalator select').val();
+        var access = $('#access select').val();
+        var information = $('#information select').val();
+        var path = $('#path select').val();
 
         //var output = '{"building":'+building+',"location":'+location+',"time":'+time+',"crowd":'+crowd+',"activatedLift":'+lift+',"activatedEscalator":'+escalator+',"activatedAccess":'+access+'}';
         //console.log(output);
 
         var output = {};
-        output["building"] = building;
-        output["location"] = location;
-        output["time"] = time;
-        output["crowd"] = crowd;
-        output["information"] = information;
-        output["path"] = path;
-        output["activatedLift"] = [];
-        output["activatedEscalator"] = [];
-        output["activatedAccess"] = [];
-        $("#escalator option").each(function() {
+        output['building'] = building;
+        output['location'] = location;
+        output['time'] = time;
+        output['crowd'] = crowd;
+        output['information'] = information;
+        output['path'] = path;
+        output['activatedLift'] = [];
+        output['activatedEscalator'] = [];
+        output['activatedAccess'] = [];
+        $('#escalator option').each(function() {
             console.log($(this).val());
             if (escalator) {
                 if (escalator.indexOf($(this).val()) == -1) {
-                    output["activatedEscalator"].push($(this).val());
+                    output['activatedEscalator'].push($(this).val());
                 }
             } else {
-                output["activatedEscalator"].push($(this).val());
+                output['activatedEscalator'].push($(this).val());
             }
         });
 
-        $("#lift option").each(function() {
+        $('#lift option').each(function() {
             //console.log($(this).val());
             if (lift) {
                 if (lift.indexOf($(this).val()) == -1) {
-                    output["activatedLift"].push($(this).val());
+                    output['activatedLift'].push($(this).val());
                 }
             } else {
-                output["activatedLift"].push($(this).val());
+                output['activatedLift'].push($(this).val());
             }
         });
 
-        $("#access option").each(function() {
+        $('#access option').each(function() {
             //console.log($(this).val());
             if (access) {
                 if (access.indexOf($(this).val()) == -1) {
-                    output["activatedAccess"].push($(this).val());
+                    output['activatedAccess'].push($(this).val());
                 }
             } else {
-                output["activatedAccess"].push($(this).val());
+                output['activatedAccess'].push($(this).val());
             }
         });
         /*
@@ -226,18 +226,18 @@ $(document).ready(function() {
         console.log(JSON.stringify(output));
         var jsonOutput = JSON.stringify(output);
         $.ajax({
-            method: "POST",
+            method: 'POST',
             url: urlStart,
             data: jsonOutput
         }).done(function(msg) {
             console.log(msg);
-            startNewSimulation(building, location, time, crowd, output["activatedLift"], output["activatedEscalator"], output["activatedAccess"]);
+            startNewSimulation(building, location, time, crowd, output['activatedLift'], output['activatedEscalator'], output['activatedAccess']);
         });
 
     });
 
-    var email = "wjloh91@gmail.com";
-    var password = "123456"
+    var email = 'wjloh91@gmail.com';
+    var password = '123456'
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
