37bed25566efa285e86e14cc70f6a5930171b284
physium <wjloh91@gmail.com>
2016-06-24 11:14:39 +0800

    updated actions for adding and removing graph via table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -40,43 +40,8 @@ var getSingleRunDataInfo = function(id) {
     return 'ID not found';
 }
 
-
-// function to update the chart with new data.
-function updateChart() {
-
-    dataTable = new google.visualization.DataTable();
-
-    var newData = [
-        ['Year', 'Sales', 'Expenses', 'Other'],
-        ['2004', 1000, 400, 232],
-        ['2005', 1170, 460, 421],
-        ['2006', 660, 1120, 4324],
-        ['2007', 1030, 540, 4234],
-        ['2008', 1530, 50, 234]
-    ];
-
-    // determine the number of rows and columns.
-    var numRows = newData.length;
-    var numCols = newData[0].length;
-
-    // in this case the first column is of type 'string'.
-    dataTable.addColumn('string', newData[0][0]);
-
-    // all other columns are of type 'number'.
-    for (var i = 1; i < numCols; i++)
-        dataTable.addColumn('number', newData[0][i]);
-
-    // now add the rows.
-    for (var i = 1; i < numRows; i++)
-        dataTable.addRow(newData[i]);
-
-    // redraw the chart.
-    chart.draw(dataTable, options);
-
-}
-
 //changes params
-$('#run input').on("change",function(){
+$('#run input').on("change paste keyup",function(){
     var noOfRuns = $(this).val();
     if(noOfRuns > 1){
         $('#batchRunParams').show();
@@ -229,7 +194,7 @@ $(document).ready(function() {
             var button = $(event.relatedTarget);
             var batchId = button.data('id');
             var batchArr = getBatchRunDataInfo(batchId);
-            console.log(batchId);
+            console.log(batchArr);
             var avgResults = averageResult(batchArr);
             console.log(avgResults)
 
@@ -280,12 +245,12 @@ $(document).ready(function() {
         var modal = $(this);
 
         modal.find('.modal-body').html("<table id='instanceTable' style='table-layout:fixed' class='table table-bordered'>" +
-            "<tr><th>Job ID</th><th>Instance ID</th><th>Instance State</th></tr>" + "</table>");
+            "<tr><th>Job ID</th><th>Instance State</th></tr>" + "</table>");
         for (var i = 0; i < batchArr.length; i++) {
             var jobId = batchArr[i]['jobId'];
             var instanceId = batchArr[i]['instanceId'];
             var instanceState = batchArr[i]['instanceState'];
-            modal.find('#instanceTable').append("<tr><td>" + jobId + "</td><<td>" + instanceId + "</td><td>" + instanceState + "</td></tr>")
+            modal.find('#instanceTable').append("<tr><td>" + jobId + "</td><<td>" + instanceState + "</td></tr>")
 
         }
 
@@ -322,8 +287,11 @@ function operateFormatter(value, row, index) {
         '<a data-toggle="modal" data-id="'+row['batchId']+'" data-target="#myModal" class="like ml10" title="Results">',
             '<i class="fa fa-search"></i>',
         '</a>',
-        '<a class="edit ml10" href="javascript:void(0)" title="Compare">',
+        '<a class="edit ml10 '+row['batchId']+'" href="javascript:void(0)" title="Compare">',
             '<i class="fa fa-plus"></i>',
+        '</a>',
+        '<a class="remove ml10 '+row['batchId']+'" href="javascript:void(0)" title="Remove" style="display:none">',
+            '<i class="fa fa-minus"></i>',
         '</a>'
     ].join('');
     // console.log(value);
@@ -356,8 +324,25 @@ window.operateEvents = {
     'click .edit': function (e, value, row, index) {
         // alert('You click edit icon, row: ' + JSON.stringify(row));
         // console.log(value, row, index);
+        
         console.log(row['batchId']);
         var selectedId = row['batchId'];
+
+        // if($('.edit.'+selectedId).is(":visible")){
+        //     $('.edit.'+selectedId).hide();
+        // }else{
+        //     $('.edit.'+selectedId).show();
+        // }
+        
+        // if($('.remove.'+selectedId).is(":visible")){
+        //     $('.remove.'+selectedId).hide();
+        // }else{
+        //     $('.remove.'+selectedId).show();
+        // }
+        
+        $(this).hide();
+        $('.remove.'+selectedId).show();
+        
         for (var i = 0; i < batchRunData.length; i++) {
             if (batchRunData[i]['batchId'] == selectedId) {
                 //var avgResults = averageResult(batchRunData[i]['batchId']);
@@ -371,9 +356,32 @@ window.operateEvents = {
                 console.log(batchArr);
             }
         }
+
         console.log(simChartData);
-        
+
         drawChart();
+       
+    },
+    'click .remove':function(e, value, row, index){
+        console.log(row['batchId']);
+        var selectedId = row['batchId'];
+
+        $(this).hide();
+        $('.edit.'+selectedId).show();
+
+        for(var i = 0; i < simChartData.length; i++){
+            if(simChartData[i]['batchId'] == selectedId){
+                console.log("remove");
+                simChartData.splice(i,1);
+                break;
+            }
+        }
+
+        if(simChartData.length > 0){
+            drawChart();
+        }else{
+            $("#simulationEvacuationChart").html("<h1>No simulations results</h1>");
+        }
     }
 
 };
